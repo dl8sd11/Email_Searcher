b26fcdb5c2f4010f3617733f317d00b48de7d335
dl8sd11 <tmd910607@gmail.com>
2021-06-22 21:56:47 +0800

    RRRR

--- main.c
+++ main.c
@@ -5,35 +5,37 @@
 #include "src/pick.h"
 #include "src/similar.h"
 #include "src/token_parser.h"
+#include "src/token_table_gen.h"
 #define HASH_TYPE long long
+clock_t prev = 0;
+
+
+void start () {
+  prev = clock();
+}
+
+#define timer(NAME) do{fprintf(stderr, #NAME);stop();}while(0);
+
+void stop () {
+  clock_t delta = clock() - prev;
+  fprintf(stderr, " Time interval of : %lf\n", (double)(delta) / CLOCKS_PER_SEC);
+}
 
 int main(void) {
 	Data data;
   Ans ans;
 	api.init(&data.n_mails, &data.n_queries, &data.mails, &data.queries);
-  qsort(data.mails, data.n_mails, sizeof(data.mails), mailIdComp);
+
+  qsort(data.mails, data.n_mails, sizeof(mail), mailIdComp);
+
+  start();
   TokenHash* mail_hash = mail_parser(&data);
-  // step 1 : remap mail_hash.hash
-  int allToken = 0;
-  for (int i = 0; i < data.n_mails; i++){
-    allToken += mailhash.len[i];
-  }
-  HASH_TYPE allTokenHash[allToken];
-  int id = 0;
-  for (int i = 0; i < data.n_mails; i++){
-    for (int j = 0; j < mail_hash.len[i]; j++)
-      allTokenHash[id++] = mail_hash.hash[i][j];
-  }
-  qsort(allTokenHash, allToken, sizeof(HASH_TYPE), hashComp);
-  int uniqueToken = 0;
-  uniqueHash(allTokenHash, allToken, &uniqueToken);
-  for (int i = 0; i < data.n_mails; i++){
-    for (int j = 0; j < mail_hash.len[i]; j++)
-      mail_hash.hash[i][j] = SearchArrayId(allTokenHash, uniqueToken, mail_hash.hash[i][j]);
-  }
-  // *allTokenHash : array storing unique hashes
-  // uniqueToken : length of the allTokenHash
-  // step 1 end
+  timer("Mail Parser");
+
+  start();
+  int uniqueTokens = reorderMailHash(mail_hash, data.n_mails);
+  timer("Reorder");
+  TokenTable* token_table = genTokenTable(mail_hash, data.n_mails, uniqueTokens);
 
   PickOrder pick_order[data.n_queries];
   SimilarGroup *similarGroups = pickSimilar(&data);
@@ -41,8 +43,9 @@ int main(void) {
   int n_mails = data.n_mails;
   for (int i=0; i<n_mails; i++) {
     if (similarGroups[i].qSz == 0) continue;
-    querySimilar(&data, &similarGroups[i], mail_hash);
+    querySimilar(&data, &similarGroups[i], mail_hash, token_table);
   }
+  timer();
 
   return 0;
 }
